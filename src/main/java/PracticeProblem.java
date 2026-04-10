7d72e9a0bb288fd81bf944cc5cdec0b3850d25da
JasonWu-ux <jasonwu544@gmail.com>
2026-04-10 19:55:50 +0000

    finish

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,12 +1,52 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
-
-	public static void q1() {
-		//Write question 1 code here
-	}
+		public static int searchMazeMoves(String[][] maze) {
+        int rows = maze.length;
+        int cols = maze[0].length;
+        
+        int startRow = rows - 1;
+        int startCol = 0;
+        
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[rows][cols];
+        
+        queue.offer(new int[]{startRow, startCol, 0}); 
+        visited[startRow][startCol] = true;
+        
+        int[][] directions = {
+            {-1, 0}, {1, 0}, {0, -1}, {0, 1}
+        };
+        
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int row = curr[0], col = curr[1], moves = curr[2];
+            
+            if (maze[row][col].equals("F")) {
+                return moves;
+            }
+            
+            for (int[] dir : directions) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+                
+                if (newRow >= 0 && newRow < rows &&
+                    newCol >= 0 && newCol < cols &&
+                    !visited[newRow][newCol] &&
+                    !maze[newRow][newCol].equals("*")) {                 
+                    visited[newRow][newCol] = true;
+                    queue.offer(new int[]{newRow, newCol, moves + 1});
+                }
+            }
+        }
+        
+        return -1; 
+    }
 
 	public static void q2() {
 		//Write question 2 code here
